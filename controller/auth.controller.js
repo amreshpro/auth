51000e486414fc0f345b52c778a388780f2ef394
Amresh Maurya <amresh.terminal@gmail.com>
2024-03-18 00:06:17 +0530

    test with database

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,24 @@
+import bcrypt from "bcrypt";
+import prisma from "../config/db.config.js";
+
 class AuthController {
-  static register(req, res) {
-    res.json(req.body);
+  static async register(req, res) {
+    const { name, email, password } = req.body;
+    if (!name && !email && !password) {
+      res.json({
+        message: "All fields are required. Please fill are detail",
+      });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: bcrypt.hashSync(password, 10),
+      },
+    });
+
+    return res.json({ message: "Account created successfully" });
   }
 }
 
